bcec49ec951e0de26a1901af0aef56331add8708
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-09-10 14:29:09 -0700

    Simplify isLegacy logic

--- core/Compatibility.js
+++ core/Compatibility.js
@@ -1,17 +1,2 @@
-export const isLegacy = function() {
-    const currentModVersion = ChatTriggers.MODVERSION.match(/^((?:\d+\.?){3})/)[1]
-    const lastSupportedVersion = "3.0.0"
-
-    const current = currentModVersion.split(".")
-    const supported = lastSupportedVersion.split(".")
-
-    for (let i = 0; i < 3; i++) {
-        let cur = current[i] | 0
-        let sup = supported[i] | 0
-        
-        if (cur !== sup) 
-            return cur < sup
-    }
-
-    return false
-}()
+/** On modern versions it's named "MOD_VERSION" */
+export const isLegacy = "MODVERSION" in ChatTriggers
